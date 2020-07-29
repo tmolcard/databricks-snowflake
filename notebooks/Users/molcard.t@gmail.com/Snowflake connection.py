c45a1dea939eaaf258c0a66ce4e71db5c91f8f69
Théophile Molcard <theophile@botify.com>
2020-07-29 16:35:43 +0200

    first connection

--- notebooks/Users/molcard.t@gmail.com/Snowflake connection.py
+++ notebooks/Users/molcard.t@gmail.com/Snowflake connection.py
@@ -1,1 +1,25 @@
 # Databricks notebook source
+# Use secrets DBUtil to get Snowflake credentials.
+user = dbutils.secrets.get("snowflake", "user")
+password = dbutils.secrets.get("snowflake", "password")
+
+# snowflake connection options
+options = {
+  "sfUrl": "wt07783.west-europe.azure.snowflakecomputing.com",
+  "sfUser": user,
+  "sfPassword": password,
+  "sfDatabase": "RESTAURANT",
+  "sfSchema": "PUBLIC",
+  "sfWarehouse": "COMPUTE_WH"
+}
+
+# COMMAND ----------
+
+# Read the data written by the previous cell back.
+df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "CUSTOMERS") \
+  .load()
+
+display(df)
