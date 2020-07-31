7d9623bdaabc9d69b61ec655c7f5d3329856f03f
Théophile Molcard <theophile@botify.com>
2020-07-31 10:12:11 +0200

    working order data loader

---
+++ notebooks/Users/molcard.t@gmail.com/load_order.py
@@ -0,0 +1,76 @@
+# Databricks notebook source
+# File storage session parameters
+
+appID = dbutils.secrets.get("datastorage", "app_id")
+secret = dbutils.secrets.get("datastorage", "secret")
+tenantID = dbutils.secrets.get("datastorage", "tenant_id")
+
+# Configure file storage session
+
+spark.conf.set("fs.azure.account.auth.type", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id", appID)
+spark.conf.set("fs.azure.account.oauth2.client.secret", secret)
+spark.conf.set("fs.azure.account.oauth2.client.endpoint", "https://login.microsoftonline.com/" + tenantID + "/oauth2/token")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+
+# COMMAND ----------
+
+# Get Snowflake credentials.
+user = dbutils.secrets.get("snowflake", "user")
+password = dbutils.secrets.get("snowflake", "password")
+
+# Snowflake connection options
+sf_options = {
+  "sfUrl": "wt07783.west-europe.azure.snowflakecomputing.com",
+  "sfUser": user,
+  "sfPassword": password,
+  "sfDatabase": "RESTAURANT",
+  "sfSchema": "PUBLIC",
+  "sfWarehouse": "COMPUTE_WH"
+}
+
+# COMMAND ----------
+
+# Storage parameters
+storageAccountName = "restaurantaccount"
+fileSystemName = "restaurant"
+
+# File parameters
+path = "raw_data/orders"
+file = "orders.csv"
+
+# Table parameters
+table_name = "ORDERS"
+
+# COMMAND ----------
+
+# Read file on the file storage
+
+df = spark.read.csv("abfss://" + fileSystemName + "@" + storageAccountName + ".dfs.core.windows.net/{path}/{file}".format(path=path, file=file), header=True)
+df.show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, unix_timestamp
+
+datetime_col = ["delivery_time", "order_accepted_time", "driver_accepted_time", "ready_for_pickup_time", "picked_up_time", "delivered_time", "delivery_date", "created_at"]
+
+for col_name in datetime_col:
+  df = df.withColumn(
+      col_name,
+      unix_timestamp(col(col_name), "yyyy-MM-dd HH:mm:ss").cast("timestamp")
+  )
+
+df.printSchema()
+
+# COMMAND ----------
+
+# Load Data Frame into table
+
+df.write \
+  .format("snowflake") \
+  .options(**sf_options) \
+  .option("dbtable", table_name) \
+  .mode("append") \
+  .save()
